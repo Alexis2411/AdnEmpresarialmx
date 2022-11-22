a124d83217d0b6df9cfcdf26a02448f3e94b60d8
alexis2411 <zs19004866@estudiantes.uv.mx>
2022-11-21 19:44:03 -0600

    first commit

---
+++ includes/function/conexion.php
@@ -0,0 +1,24 @@
+<?php
+// DEV
+/*
+    define('HOST_DB','localhost:9006');
+    define('USER_DB','root');
+    define('PASS_DB','12334');
+    define('NAME_DB','encuestauv');
+*/
+
+// PROD
+
+    define('HOST_DB','localhost');
+    define('USER_DB','adnempre_admin');
+    define('PASS_DB','Uv2020@#$');
+    define('NAME_DB','adnempre_encuesta');
+
+    $con = new mysqli(HOST_DB,USER_DB,PASS_DB,NAME_DB);
+    
+    if ($con->connect_error) {
+        echo $con->connect_error;
+    }
+    
+    $con->set_charset('utf8');
+?>
---
+++ includes/function/database.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ 	--------------------------
+	Autor: Obed Alvarado
+	Web: obedalvarado.pw
+	Mail: info@obedalvarado.pw
+	----------------------------
+ 
+*/
+class database{
+	//DEV
+	/*
+	private $host='localhost:9006';//generalmente suele ser "127.0.0.1"
+	private $user='root';//Usuario de tu base de datos
+	private $pass='12334';//Contraseña del usuario de la base de datos
+	private $db='encuestauv';//Nombre de la base de datos
+	public $counter;//Propiedad para almacenar el numero de registro devueltos por la consulta
+*/
+
+
+	//PROD
+
+	private $host='localhost';//generalmente suele ser "127.0.0.1"
+	private $user='adnempre_admin';//Usuario de tu base de datos
+	private $pass='Uv2020@#$';//Contraseña del usuario de la base de datos
+	private $db='adnempre_encuesta';//Nombre de la base de datos
+	public $counter;//Propiedad para almacenar el numero de registro devueltos por la consulta
+
+	public  function conectar(){
+		$conexion = new mysqli($this->host, $this->user, $this->pass, $this->db);
+		$conexion->query("SET NAMES 'utf8'");
+		return $conexion;
+	}
+}
+?>
+
